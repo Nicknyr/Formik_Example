da666fedf61671df41758ef1e477af63c98c07c9
Nicknyr <nick@Nicks-MacBook-Pro.local>
2019-10-28 15:21:36 -0400

    Handled onSubmit functionality and error messages when form inputs are invalid

--- src/ContactForm.js
+++ src/ContactForm.js
@@ -5,37 +5,62 @@ import { Formik, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 
 const CONTAINER = styled.div`
-  background: #778beb;
+  background: #F7F9FA;
   height: auto;
-  width: 80%;
+  width: 60%;
   margin: 10em auto;
   color: snow;
+  -webkit-box-shadow: 5px 5px 5px 0px rgba(0, 0, 0, 0.4);
+  -moz-box-shadow: 5px 5px 5px 0px rgba(0, 0, 0, 0.4);
+  box-shadow: 5px 5px 5px 0px rgba(0, 0, 0, 0.4);
 
   label {
-    font-size: 1.4em;
+    color: #24B9B6;
+    font-size: 1.2em;
     font-weight: 400;
   }
 
-  .has-error {
-    border: 2px solid #c44569;
+  .error {
+    border: 2px solid #FF6565;
+  }
+
+  .error-message {
+    color: #FF6565;
+    padding: .5em .2em;
+    height: 1em;
+    position: absolute;
+    font-size: .8em;
+  }
+
+  h1 {
+    color: #24B9B6;
+    padding-top: .5em;
+  }
+
+  .form-group {
+    margin-bottom: 2.5em;
   }
 `;
 
 const MYFORM = styled(Form)`
-  width: 40%;
+  width: 90%;
   text-align: left;
-  padding-top: 5em;
-  padding-bottom: 5em;
+  padding-top: 2em;
+  padding-bottom: 2em;
+
+  @media(min-width: 786px) {
+    width: 50%;
+  }
 `;
 
 const BUTTON = styled(Button)`
-  background: #63cdda;
+  background: #1863AB;
   border: none;
   font-size: 1.2em;
   font-weight: 400;
 
   &:hover {
-    background: #3dc1d3;
+    background: #1D3461;
   }
 `;
 
@@ -47,28 +72,44 @@ const validationSchema = Yup.object().shape({
   name: Yup.string()
   .min(2, "Names must has at least 2 characters")
   .max(100, "Names can't be longer than 100 characters")
-  .required("Name is required"),
+  .required("*Name is required"),
   email: Yup.string()
   .email("Must be a valid email address")
   .max(100, "Email must be less than 100 characters")
-  .required("Email is required"),
+  .required("*Email is required"),
   phone: Yup.string()
   .matches(phoneRegExp, "Phone number is not valid")
-  .required("Phone number required"),
+  .required("*Phone number required"),
   blog: Yup.string()
   .url("Must enter URL")
-  .required("URL required")
+  .required("*URL required")
 });
 
 const ContactForm = () => {
   return(
     <CONTAINER>
+    <h1>Example Formik Form</h1>
     <Formik
       initialValues={{ name:"", email:"", phone:"", blog:""}}
       validationSchema={validationSchema}
+      onSubmit={(values, {setSubmitting, resetForm}) => {
+          setSubmitting(true);
+
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+            resetForm();
+            setSubmitting(false);
+          }, 500);
+      }}
     >
-      {( {values, errors, touched, handleChange, handleBlur }) => (
-        <MYFORM className="mx-auto">
+      {( {values,
+          errors,
+          touched,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          isSubmitting }) => (
+        <MYFORM onSubmit={handleSubmit} className="mx-auto">
           <Form.Group controlId="formName">
             <Form.Label>Name :</Form.Label>
             <Form.Control
@@ -80,6 +121,9 @@ const ContactForm = () => {
               value={values.name}
               className={touched.name && errors.name ? "has-error" : null}
               />
+              {touched.name && errors.name ? (
+                <div className="error-message">{errors.name}</div>
+              ): null}
           </Form.Group>
           <Form.Group controlId="formEmail">
             <Form.Label>Email :</Form.Label>
@@ -92,6 +136,9 @@ const ContactForm = () => {
               value={values.email}
               className={touched.email && errors.email ? "has-error" : null}
                />
+               {touched.email && errors.email ? (
+                 <div className="error-message">{errors.email}</div>
+               ): null}
           </Form.Group>
           <Form.Group controlId="formPhone">
             <Form.Label>Phone :</Form.Label>
@@ -104,6 +151,9 @@ const ContactForm = () => {
               value={values.phone}
               className={touched.phone && errors.phone ? "has-error" : null}
                />
+             {touched.phone && errors.phone ? (
+                 <div className="error-message">{errors.phone}</div>
+               ): null}
           </Form.Group>
           <Form.Group controlId="formBlog">
             <Form.Label>Blog :</Form.Label>
@@ -116,9 +166,12 @@ const ContactForm = () => {
               value={values.blog}
               className={touched.blog && errors.blog ? "has-error" : null}
               />
+            {touched.blog && errors.blog ? (
+                <div className="error-message">{errors.blog}</div>
+              ): null}
           </Form.Group>
 
-          <BUTTON variant="primary" type="submit">
+          <BUTTON variant="primary" type="submit" disabled={isSubmitting}>
             Submit
           </BUTTON>
         </MYFORM>
